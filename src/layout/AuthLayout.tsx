94c7865a069c78398994bf77d7e3df3742c69c2b
Ryan Matheus <ryanmatheusdev@gmail.com>
2024-09-26 16:07:24 -0300

    feat: novos componentes

--- src/layout/AuthLayout.tsx
+++ src/layout/AuthLayout.tsx
@@ -4,8 +4,6 @@ import Login from "../pages/Login";
 export default function AuthLayout() {
   const location = useLocation();
   return (
-    <div className="main">
-      {location.pathname == "/auth/login" && <Login />}
-    </div>
+    <div className="main">{location.pathname == "/login" && <Login />}</div>
   );
 }
